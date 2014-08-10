4c134f77d3509b5c48963d81c436d96342baa1a7
Gustavo <gustavoyama@gmail.com>
2014-08-10 01:39:13 -0300

    Revert "parte2, chance 2"
    This reverts commit 93e2db195ceb6cc1e3126b41c24b64389dc28943.

--- app/models/Meta.java
+++ app/models/Meta.java
@@ -1,33 +1,38 @@
 package models;
 
+import java.util.*;
+
 import javax.persistence.*;
 
+import play.data.Form;
 import play.data.validation.Constraints.*;
+import play.db.ebean.*;
+import play.mvc.Content;
 
-
-@Entity(name="Meta")
-public class Meta{
+@Entity
+public class Meta extends Model implements Comparable<Meta>{
 	
 	@Id @GeneratedValue
 	private Long id;
 
-	@Transient
-	private final String PENDENTE = "pendente";
-	@Transient
-	private final String CONCLUIDA = "concluida";
+	private final int PENDENTE = 0;
+	private final int ALCANCADA = 1;
 
-	@Required
+	@OneToOne(mappedBy = "Meta")
+	@JoinColumn
 	private String descricao;
-	private String situacao = PENDENTE;
+	private int situacao;
 	private int prioridade;
 	private int semana;
+	
+	public static Finder<Long, Meta> find = new Finder<Long, Meta>(Long.class, Meta.class);
 	
 	public int getSemana() {
 		return semana;
 	}
 
 	public void setSemana(int semana) {
-		if(semana>=1 || semana <=6)
+		if(semana> 0 && semana <7)
 			this.semana = semana;
 	}
 
@@ -36,6 +41,7 @@ public class Meta{
 	public Meta(String descricao, int prioridade, int semana){
 		situacao = PENDENTE;
 		this.descricao = descricao;
+		setSemana(semana);
 		setPrioridade(prioridade);
 	}
 
@@ -52,6 +58,10 @@ public class Meta{
 		return id;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getDescricao() {
 		return descricao;
 	}
@@ -59,21 +69,33 @@ public class Meta{
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
-	public String getSituacao() {
+
+	public int getSituacao() {
 		return situacao;
 	}
 
-	public void setSituacao(String situacao) {
-		this.situacao = situacao.toLowerCase();
+	public void setSituacao(int situacao) {
+		this.situacao = situacao;
 	}
 
-	public String getPENDENTE() {
+	public int getPENDENTE() {
 		return PENDENTE;
 	}
 
-	public String getCONCLUIDA() {
-		return CONCLUIDA;
+	public int getALCANCADA() {
+		return ALCANCADA;
+	}
+	
+	public static List<Meta> all() {
+	  return find.all();
+	}
+
+	public static void create(Meta meta) {
+	  meta.save();
+	}
+
+	public static void delete(Long id) {
+	  find.ref(id).delete();
 	}
 
 	@Override
@@ -85,8 +107,27 @@ public class Meta{
 		return false;
 	}
 
+	public String getSitucaoString(){
+		if(situacao == PENDENTE){
+			return "pendente";
+		}
+		else if(situacao ==ALCANCADA){
+			return "alcancada";
+		}
+		else return "";
+	}
+
 	@Override
 	public String toString(){
-		return "Id: " + getId() + "; Descricao: " + getDescricao() + "; Situcao: " + getSituacao() + "; Semana: " + getSemana() + "; Prioridade: " + getPrioridade() +"\n";
+		return "Id: " + getId() + "; Descricao: " + getDescricao() + "; Situcao: " + getSitucaoString();
+	}
+
+	@Override
+	public int compareTo(Meta outraMeta) {
+		if(prioridade > outraMeta.getPrioridade())
+			return -1;
+		else if(prioridade > outraMeta.getPrioridade())
+			return 1;
+		return 0;
 	}
 }
